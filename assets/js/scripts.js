244a922f1c43e11ff9100090696c07d8d572e001
Marcel Sproesser <87661745+marcelsproesser@users.noreply.github.com>
2022-06-21 16:05:26 -0300

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    if (currentNumber >= 11) {
+        currentNumber = currentNumber - 1;
+    }
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    if(-1 >= currentNumber) {
+        currentNumber = currentNumber + 1;    
+}
+    currentNumberWrapper.innerHTML = currentNumber;
+}
